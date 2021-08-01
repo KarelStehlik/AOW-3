894ca78a7e3b29252cbda99d3a81f31a64f324d2
KarelStehlik <karel.stehlik.cz@gmail.com>
2021-08-01 16:57:18 +0200

    dfg

--- server_main.py
+++ server_main.py
@@ -64,8 +64,8 @@ class cw_server(Server):
         [e.tick() for e in self.games]
 
 
-#srvr = cw_server(localaddr=("192.168.1.237", 5071))
-srvr = cw_server(localaddr=("127.0.0.1", 5071))
+srvr = cw_server(localaddr=("192.168.1.237", 5071))
+#srvr = cw_server(localaddr=("127.0.0.1", 5071))
 
 #pyglet.clock.schedule_interval(srvr.tick, 1.0 / constants.FPS)
 while True:
